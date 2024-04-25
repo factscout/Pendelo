53e16cc868e93175c606a2e0c98ad1b5b7309e12
Noah Howald <n@rootsconnection.ch>
2024-04-25 23:41:25 +0200

    Login Controller

--- Backend/Controllers/LoginController.cs
+++ Backend/Controllers/LoginController.cs
@@ -1,6 +1,50 @@
-﻿namespace Backend.Controllers
+﻿using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Mysqlx;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Controllers
 {
-    public class LoginController
+    [Route("api/[controller]")]
+    public class UserLoginController : Controller
     {
+        [HttpPost("login")]
+        public IActionResult CheckLogin([FromBody] NetUserLogin value) {
+
+            DBResult result = Program.DB.CheckEmail(value.Email);
+
+            if (!result.success) return BadRequest("Email wird nicht verwendet");
+
+
+            bool? check = Program.DB.CheckLogin(value.Email, value.Password);
+
+            if (check == null) {
+           
+                Console.WriteLine("LoginController: Password Check Failed");
+                return BadRequest("Password Check Failed");
+            }
+
+            if (!(bool)check) {
+                return BadRequest("Passwort ist falsch!");
+            }
+
+            DBResult getid = Program.DB.GetID(value.Email);
+            return Ok(getid.result);
+
+        }
+
+        public class NetUserLogin
+        {
+            [Required] public string Email { get; set; }
+            [Required] public string Password { get; set; }
+
+            public NetUserLogin(string email, string password) {
+                this.Email = email;
+                this.Password = password;
+            }
+
+            public NetUserLogin() { }
+
+        }
     }
 }
